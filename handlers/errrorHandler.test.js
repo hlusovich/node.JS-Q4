fa256921c012dffc41839bccc654f76f11b97e19
hlusovich <bigklu95@gmail.com>
2021-11-17 21:57:21 +0300

    add test for errorHandler

--- handlers/errrorHandler.test.js
+++ handlers/errrorHandler.test.js
@@ -1,10 +1,10 @@
 const {expect} = require("@jest/globals");
 const errorHandler = require('./errorHandler');
-const myError = new Error("test error handler");
+const myError = new Error("test for error handler");
 
 test("should return Error", () => {
     expect(() => errorHandler(myError)).toThrow(myError);
 });
-test("should return Errors text", () => {
-    expect(() => errorHandler(myError)).toThrow("test error handler");
+test("should return correct Errors text", () => {
+    expect(() => errorHandler(myError)).toThrow("test for error handler");
 });
